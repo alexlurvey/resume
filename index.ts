1a2df5fc6f5fb2acb042227979a68a6f777d77a1
alexlurvey <alex.lurvey@gmail.com>
2020-03-23 10:43:20 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import * as Koa from 'koa';
+import * as Router from '@koa/router';
+import { readFileSync } from 'fs';
+
+const app = new Koa()
+const router = Router()
+
+router.get('/resume', (ctx, next) => {
+  const pdf = readFileSync('resume.pdf')
+  ctx.set('Content-Type', 'application/pdf')
+  ctx.body = pdf;
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .listen(3000)
