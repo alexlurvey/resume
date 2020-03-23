0520e63dc35bb6c763fd72cf98057d0baae8a65d
alexlurvey <alex.lurvey@gmail.com>
2020-03-23 11:29:05 -0500

    rename resume file to avoid name conflict with ts file

--- api/resume.ts
+++ api/resume.ts
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 
 const handler = async (_req, res) => {
-  const pdf = readFileSync('resume.pdf')
+  const pdf = readFileSync('my_resume.pdf')
   res.status(200)
     .type('application/pdf')
     .send(pdf)
