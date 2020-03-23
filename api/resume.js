2a66b6ef5853c8f8fda4dc1113ba2ab25c0bdf27
alexlurvey <alex.lurvey@gmail.com>
2020-03-23 11:36:14 -0500

    fix typo

--- api/resume.js
+++ api/resume.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
-module.exports = async (_req, res) => {
+module.exports = (_req, res) => {
   const pdf = fs.readFileSync('my_resume.pdf')
   res.status(200)
     .type('application/pdf')
     .send(pdf)
-})
+}
